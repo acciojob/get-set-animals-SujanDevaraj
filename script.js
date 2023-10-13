b705f6e5704e86adc39b2262280da595aad47a01
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-10-13 21:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 //complete this code
 class Animal {
-	constructor (species){
-		this._species =species
-	}
-
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		console.log("The Golden Retriever makes a sound")
-	}
-}
-
-class Dog extends Animal {
-
-	bark(){
-		console.log("woof")
-	}
-}
-
-class Cat extends Animal {
   constructor(species) {
-    super(species);
+    this._species = species;
   }
 
-  purr() {
-    console.log("purr");
+  get species() {
+    return this._species;
   }
 
   makeSound() {
-    console.log(`The ${this.species} makes a sound`);
+    console.log('The " + this.species + " makes a sound');
   }
 }
+
+class Cat extends Animal {
+  purr() {
+    console.log('purr');
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log('woof');
+  }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
